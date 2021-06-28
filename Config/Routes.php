f2546cd26be9eebf8fd581a06146ac86d1991096
ridho146 <56241745+ridho146@users.noreply.github.com>
2021-06-28 18:03:14 +0700

    Add files via upload

--- Config/Routes.php
+++ Config/Routes.php
@@ -36,6 +36,14 @@ $routes->get('/faqs', 'Page::faqs');
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/artikel/(:any)', 'Artikel::view/$1');
+$routes->group('admin', function($routes) {
+	$routes->get('artikel', 'Artikel::admin_index');
+	$routes->add('artikel/add', 'Artikel::add');
+	$routes->add('artikel/edit/(:any)', 'Artikel::edit/$1');
+	$routes->get('artikel/delete/(:any)', 'Artikel::delete/$1');
+  });
+  
 
 /*
  * --------------------------------------------------------------------
